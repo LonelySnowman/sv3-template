9ad655e1b82d3c8557b0bf83f22e4adc6dfef0a3
LonelySnowman <2438490168@qq.com>
2023-04-28 08:26:15 +0800

    feat: 封装深拷贝函数

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -21,3 +21,59 @@ export function throttle(fn: () => void, delay = 200): () => void {
       }
    };
 }
+
+export function deepClone(target) {
+   const hashMap = new WeakMap();
+   function isObject(target) {
+      return (
+         (typeof target == 'object' && target !== null) ||
+         typeof target == 'function'
+      );
+   }
+   function clone(target) {
+      if (!isObject(target)) return target;
+      if (hashMap.has(target)) return hashMap.get(target);
+      if ([Date, RegExp].includes(target.constructor)) {
+         return new target.constructor(target);
+      }
+      if (target instanceof Function) {
+         return new Function('return ' + target.toString())();
+      }
+      if (target instanceof Map) {
+         const newMap = new Map();
+         for (const [key, val] of target) {
+            if (isObject(val)) newMap.set(key, clone(val));
+            else newMap.set(key, val);
+         }
+         return newMap;
+      }
+      if (target instanceof Set) {
+         const newSet = new Set();
+         for (const val of target) {
+            if (isObject(val)) newSet.add(clone(val));
+            else newSet.add(val);
+         }
+         return newSet;
+      }
+      if (target instanceof Array) {
+         const n = target.length;
+         const newArray = new Array(n);
+         for (let i = 0; i < n; i++) {
+            const item = target[i];
+            if (isObject(item)) newArray[i] = clone(item);
+            else newArray[i] = item;
+         }
+         return newArray;
+      }
+      const keys = Reflect.ownKeys(target);
+      const newObj = {};
+      for (const key of keys) {
+         const val = target[key];
+         if (isObject(val)) newObj[key] = clone(val);
+         else newObj[key] = val;
+      }
+      hashMap.set(target, newObj);
+      return newObj;
+   }
+   return clone(target);
+}
