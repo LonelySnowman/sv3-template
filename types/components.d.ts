ddfe463a6bca51d2fd4e038956e73e952124ae4a
LonelySnowman <2438490168@qq.com>
2023-11-10 17:25:16 +0800

    style: 剔除冗余代码

--- types/components.d.ts
+++ types/components.d.ts
@@ -5,29 +5,29 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {};
+export {}
 
 declare module '@vue/runtime-core' {
-   export interface GlobalComponents {
-      ElButton: typeof import('element-plus/es')['ElButton'];
-      ElContainer: typeof import('element-plus/es')['ElContainer'];
-      ElDivider: typeof import('element-plus/es')['ElDivider'];
-      ElDrawer: typeof import('element-plus/es')['ElDrawer'];
-      ElForm: typeof import('element-plus/es')['ElForm'];
-      ElFormItem: typeof import('element-plus/es')['ElFormItem'];
-      ElHeader: typeof import('element-plus/es')['ElHeader'];
-      ElInput: typeof import('element-plus/es')['ElInput'];
-      ElMain: typeof import('element-plus/es')['ElMain'];
-      ElMenu: typeof import('element-plus/es')['ElMenu'];
-      ElMenuItem: typeof import('element-plus/es')['ElMenuItem'];
-      ElSwitch: typeof import('element-plus/es')['ElSwitch'];
-      IEpClose: typeof import('~icons/ep/close')['default'];
-      IEpDocument: typeof import('~icons/ep/document')['default'];
-      IEpEdit: typeof import('~icons/ep/edit')['default'];
-      IEpRight: typeof import('~icons/ep/right')['default'];
-      IEpSetting: typeof import('~icons/ep/setting')['default'];
-      IEpUser: typeof import('~icons/ep/user')['default'];
-      RouterLink: typeof import('vue-router')['RouterLink'];
-      RouterView: typeof import('vue-router')['RouterView'];
-   }
+  export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
+    ElDrawer: typeof import('element-plus/es')['ElDrawer']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
+    IEpClose: typeof import('~icons/ep/close')['default']
+    IEpDocument: typeof import('~icons/ep/document')['default']
+    IEpEdit: typeof import('~icons/ep/edit')['default']
+    IEpRight: typeof import('~icons/ep/right')['default']
+    IEpSetting: typeof import('~icons/ep/setting')['default']
+    IEpUser: typeof import('~icons/ep/user')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
 }
